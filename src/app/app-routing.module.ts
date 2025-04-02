bc1a1672f1c94c27aa3844abbb466b67e5d4b165
abel hv <115476506+abelHV@users.noreply.github.com>
2025-04-02 19:49:50 +0200

    appModule/crearRessenya-buscUser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { BuscadorCursosComponent } from './buscador-cursos/buscador-cursos.component';
 import { PerfilPropioComponent } from './perfil-propio/perfil-propio.component';
+import { BusadorUsuarioComponent } from './busador-usuario/busador-usuario.component';
+import { CrearResenyaComponent } from './crear-resenya/crear-resenya.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },  // Página principal
   { path: 'register', component: RegisterComponent },
   { path: 'buscadorCursos', component: BuscadorCursosComponent } ,
-  { path: 'perfilPropio', component: PerfilPropioComponent } // Página de perfil propio
+  { path: 'perfilPropio', component: PerfilPropioComponent } ,// Página de perfil propio
+  { path: 'buscadorUsuario', component: BusadorUsuarioComponent }, // Página de perfil propio
+  { path: 'CrearRessenya', component: CrearResenyaComponent }, // Página de perfil propio
+
+
 
 ];
 
